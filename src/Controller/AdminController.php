94d225a00187b855507078eca67c63c1555cedd8
cymard <cyriansimard@gmail.com>
2021-04-27 12:09:40 +0200

    clean code DRY pour 'AdminController'

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,6 +13,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
+    private $request;
+    private $paginator;
+
+    public function __construct(Request $request, PaginatorInterface $paginator)
+    {
+        $this->request = $request;
+        $this->paginator = $paginator;
+    }
 
     private function getCategory (string $category) 
     {
@@ -25,12 +33,33 @@ class AdminController extends AbstractController
         }
     }
 
+    private function sendJsonResponse ($dataJsonFormat)
+    {
+        $response = new JsonResponse();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent($dataJsonFormat);
+    
+        return $response;
+    }
+
+    private function queryPaginator ($query, $page, $productsPerPage)
+    {
+        $articles = $this->paginator->paginate(
+            $query, // Requête contenant les données à paginer (ici nos articles)
+            $this->request->query->getInt('page', $page), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            $productsPerPage // Nombre de résultats par page
+
+        );
+
+        return $articles;
+    }
+
     
     /**
      * @Route("/admin/home", name="admin_product_category", methods={"GET"})
      * Display the products per page from a specific category
      */
-    public function getCategoryProducts (Request $request,ProductRepository $productRepository,  PaginatorInterface $paginator, NormalizerInterface $normalizerInterface)
+    public function getCategoryProducts (Request $request,ProductRepository $productRepository, NormalizerInterface $normalizerInterface)
     {
         if($request->query->get('search') && $request->query->get('category') && $request->query->get('page') && $request->query->get('sorting')){
 
@@ -50,26 +79,15 @@ class AdminController extends AbstractController
             
             $pageNumber = ceil ($allProducts/$productsPerPage);
             
-            $articles = $paginator->paginate(
-                $query, // Requête contenant les données à paginer (ici nos articles)
-                $request->query->getInt('page', $page), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-                $productsPerPage // Nombre de résultats par page
-
-            );
+            $articles = $this->queryPaginator($query, $page, $productsPerPage);
             
             // conversion des objets en tableaux
             $array = $normalizerInterface->normalize($articles,null,["groups" => "productWithoutComments"]);
 
-            
-            $response = new JsonResponse();
-            $response->headers->set('Content-Type', 'application/json');
-
             // conversion des tableaux en json
             $allResponses = json_encode(["productsPerPageNumber" => $productsPerPage,"search"=> $search ,"allProductsNumber" => $allProducts, "totalPageNumber"=>$pageNumber, "pageContent"=>$array]);
 
-            $response->setContent($allResponses);
-
-            return $response;
+            return $this->sendJsonResponse($allResponses);
 
 
         }else if($request->query->get('category') && $request->query->get('page') && $request->query->get('sorting') ){
@@ -84,32 +102,20 @@ class AdminController extends AbstractController
             $allProducts = count($query->getResult());
             $productsPerPage = 9;
             $pageNumber = ceil ($allProducts/$productsPerPage);
-            
+        
 
-            $articles = $paginator->paginate(
-                $query, // query
-                $request->query->getInt('page', $page), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-                $productsPerPage // Nombre de résultats par page
-            );
+            $articles = $this->queryPaginator($query, $page, $productsPerPage);
 
             // conversion des objets en tableaux
             $array = $normalizerInterface->normalize($articles,null,["groups" => "productWithoutComments"]);
- 
-            // responses
-            $response = new JsonResponse();
-            $response->headers->set('Content-Type', 'application/json');
 
             // conversion des tableaux en json
             $allResponses = json_encode(["productsPerPageNumber" => $productsPerPage,"category"=> $category ,"allProductsNumber" => $allProducts, "totalPageNumber"=>$pageNumber, "pageContent"=>$array]);
 
-            $response->setContent($allResponses);
-
-            return $response;
+            return $this->sendJsonResponse($allResponses);
 
         }else{
-
             return $this->json(['status' => 400, 'message' => "Les paramètres de l'url ne sont pas valides."]);
-
         }
 
     }
@@ -130,11 +136,158 @@ class AdminController extends AbstractController
             $entityManager->flush();
         }
 
-        $response = new JsonResponse(['status' => 202,'message' => 'All products deleted']);
-        return $response;
+        return $this->sendJsonResponse(['status' => 202,'message' => 'All products deleted']);
     }
 
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class AdminController extends AbstractController
+// {
+
+//     private function getCategory (string $category) 
+//     {
+//         if( $category === "sports"){
+//             return "sports/vetements";
+//         }else if($category === "informatique"){
+//             return "informatique/high-tech";
+//         }else{
+//             return $category;
+//         }
+//     }
+
+
+
+    
+//     /**
+//      * @Route("/admin/home", name="admin_product_category", methods={"GET"})
+//      * Display the products per page from a specific category
+//      */
+//     public function getCategoryProducts (Request $request,ProductRepository $productRepository,  PaginatorInterface $paginator, NormalizerInterface $normalizerInterface)
+//     {
+//         if($request->query->get('search') && $request->query->get('category') && $request->query->get('page') && $request->query->get('sorting')){
+
+//             // récuperer la valeur de la recherche
+//             $search =  $request->query->get('search');
+//             $page = (int)$request->query->get('page');
+//             $sort = $request->query->get('sorting');
+//             $category =  $this->getCategory($request->query->get('category'));
+
+//             // faire la recherche
+//             $data = $productRepository->searchProductAdmin($search,$category, $sort);
+           
+//             $query = $data->getQuery();
+        
+//             $productsPerPage = 9;
+//             $allProducts = count($query->getResult());
+            
+//             $pageNumber = ceil ($allProducts/$productsPerPage);
+            
+//             $articles = $paginator->paginate(
+//                 $query, // Requête contenant les données à paginer (ici nos articles)
+//                 $request->query->getInt('page', $page), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+//                 $productsPerPage // Nombre de résultats par page
+
+//             );
+            
+//             // conversion des objets en tableaux
+//             $array = $normalizerInterface->normalize($articles,null,["groups" => "productWithoutComments"]);
+
+            
+//             $response = new JsonResponse();
+//             $response->headers->set('Content-Type', 'application/json');
+
+//             // conversion des tableaux en json
+//             $allResponses = json_encode(["productsPerPageNumber" => $productsPerPage,"search"=> $search ,"allProductsNumber" => $allProducts, "totalPageNumber"=>$pageNumber, "pageContent"=>$array]);
+
+//             $response->setContent($allResponses);
+
+//             return $response;
+
+
+//         }else if($request->query->get('category') && $request->query->get('page') && $request->query->get('sorting') ){
+//             $sort = $request->query->get('sorting');
+//             $category =  $this->getCategory($request->query->get('category'));
+//             $page = (int)$request->query->get('page');
+
+//             $data = $productRepository->searchProductAdminWithCategory($category,$sort);
+
+//             $query = $data->getQuery();
+
+//             $allProducts = count($query->getResult());
+//             $productsPerPage = 9;
+//             $pageNumber = ceil ($allProducts/$productsPerPage);
+            
+
+//             $articles = $paginator->paginate(
+//                 $query, // query
+//                 $request->query->getInt('page', $page), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+//                 $productsPerPage // Nombre de résultats par page
+//             );
+
+//             // conversion des objets en tableaux
+//             $array = $normalizerInterface->normalize($articles,null,["groups" => "productWithoutComments"]);
+ 
+//             // responses
+//             $response = new JsonResponse();
+//             $response->headers->set('Content-Type', 'application/json');
+
+//             // conversion des tableaux en json
+//             $allResponses = json_encode(["productsPerPageNumber" => $productsPerPage,"category"=> $category ,"allProductsNumber" => $allProducts, "totalPageNumber"=>$pageNumber, "pageContent"=>$array]);
+
+//             $response->setContent($allResponses);
+
+//             return $response;
+
+//         }else{
+
+//             return $this->json(['status' => 400, 'message' => "Les paramètres de l'url ne sont pas valides."]);
+
+//         }
+
+//     }
+
+//     /**
+//      * @Route("/admin/product", name="admin_delete_product", methods={"DELETE"})
+//      * Delete products
+//      */
+//     public function deleteProduct(Request $request, ProductRepository $repo, EntityManagerInterface $entityManager){
+//         $json = $request->getContent();
+//         $idToDelete = json_decode ( $json );
+
+//         foreach($idToDelete->data as $id ){
+//             // identifier le produit
+//             $product = $repo->find($id);
+
+//             $entityManager->remove($product);
+//             $entityManager->flush();
+//         }
+
+//         $response = new JsonResponse(['status' => 202,'message' => 'All products deleted']);
+//         return $response;
+//     }
+
+
+
+
+// }
